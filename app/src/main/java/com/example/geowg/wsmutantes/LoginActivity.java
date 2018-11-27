1fcb3fa584cca1bdfad90430d4257cd26d92f893
Felipe Moreno <felipemorenolopes@gmail.com>
2018-11-26 22:44:02 -0200

    Início da LoginActivity Criação das activities: CadastrarActivity ListarActivity PesquisarActivity

--- app/src/main/java/com/example/geowg/wsmutantes/LoginActivity.java
+++ app/src/main/java/com/example/geowg/wsmutantes/LoginActivity.java
@@ -1,13 +1,72 @@
 package com.example.geowg.wsmutantes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etUser, etPassword;
+    Button btnLogin;
+    String URL = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUser = findViewById(R.id.etUser);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        if (s.equals("true")) {
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this, "Some error ocurred -> " + error, Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError{
+                        Map<String, String> parameters = new HashMap<String, String>();
+                        parameters.put("user", etUser.getText().toString());
+                        parameters.put("password", etPassword.getText().toString());
+                        return parameters;
+                    }
+                };
+
+                RequestQueue rQueue = Volley.newRequestQueue(LoginActivity.this);
+                rQueue.add(request);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/geowg/wsmutantes/MainActivity.java
+++ app/src/main/java/com/example/geowg/wsmutantes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.geowg.wsmutantes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Inicia activity de cadastro de mutante
+    public void cadastrarMutante(View view){
+        Intent it = new Intent(this, CadastrarActivity.class);
+        startActivity(it);
+    }
+
+    //Inicia activity com a lista dos mutantes cadastrados
+    public void listarMutante(View view){
+        Intent it = new Intent(this, ListarActivity.class);
+        startActivity(it);
+    }
+
+    //Inicia activity para pesquisa de mutantes
+    public void pesquisarMutante(View view){
+        Intent it = new Intent(this, PesquisarActivity.class);
+        startActivity(it);
+    }
+
+    //Fecha o aplicativo
+    public void fecharApp(View view){
+        System.exit(0);
     }
 }
